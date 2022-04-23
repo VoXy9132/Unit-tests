d0e103600451c67441fd75a20da2c6f9b64afc74
VoXy9132 <voxy9132@gmail.com>
2022-04-23 20:39:58 +0300

    tests update

--- tests.py
+++ tests.py
@@ -1,13 +1,12 @@
 import unittest
 import calc
-
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(calc.add(3, 7), 10)
 
     def test_is_positive(self):
-        self.asserTrue(calc.is_positive(3))
+        self.assertTrue(calc.is_positive(3))
 
     def test_divide(self):
         with self.assertRaises(ZeroDivisionError):
@@ -17,5 +16,11 @@ class TestCalc(unittest.TestCase):
         self.assertEqual('abc'.upper(), 'ABC')
 
     def test_strip(self):
-        self.assertEqual('abc '.strip(), 'abc')
-        
+        with self.assertRaises(AttributeError):
+            x = 42
+            x.split()
+
+
+
+
+
